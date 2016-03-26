87d32beb1cd9eac431e8aecd2c7c134f7067f6ca
rduke <kaminskr@my.erau.edu>
2016-03-26 14:45:00 -0400

    Push new module

---
+++ JuiceBox/src/edu/erau/hackathon/SoundBox.java
@@ -0,0 +1,14 @@
+package edu.erau.hackathon;
+
+import javafx.application.Application;
+
+/**
+ * Created by rduke on 3/26/16.
+ */
+public class SoundBox extends GUI {
+    public static void main(String[] args) {
+        GUI.launch();
+
+
+    }
+}
