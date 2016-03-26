ddcaefb48f7e3d08a75922b1e129d52020450f0b
rduke <kaminskr@my.erau.edu>
2016-03-26 14:35:18 -0400

    Working SoundCloud API example

---
+++ Robby/src/com/robbyduke/soundapp/player/Player.java
@@ -0,0 +1,17 @@
+package com.robbyduke.soundapp.player;
+
+/**
+ * HackRiddle hackathon 03/26/2016
+ * --
+ * Team Members
+ * - @author Robert Duke
+ * - @author John Martincic
+ * - @author Ryan Newitt
+ * - @author Andy Artze
+
+ * This application is designed to replace the annoying web-only interface of SoundCloud on the desktop.
+ */
+
+public class Player {
+
+}
